7d4fec109538e176da4b993d476b321c701c26a2
m1nuz <xminuz@gmail.com>
2017-08-18 09:11:47 +0300

    Add config & install

--- install.sh
+++ install.sh
@@ -25,4 +25,12 @@ chsh -s $(which zsh)
 # Install http://ohmyz.sh/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Install rofi
+sudo apt-get install rofi -y
+mkdir ~/.config/rofi
+curl -o ~/.config/rofi/config https://raw.githubusercontent.com/DaveDavenport/rofi-themes/master/Themes/Arc-Dark.theme
+
+sudo apt-get install xcompmgr -y
+
+
 
