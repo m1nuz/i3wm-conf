af88b534fdc820277e4fdbe7d92f80d9b4ec87ba
m1nuz <xminuz@gmail.com>
2017-08-22 00:30:38 +0300

    Update

--- install.sh
+++ install.sh
@@ -18,12 +18,18 @@ mkdir ~/.fonts
 wget https://github.com/FortAwesome/Font-Awesome/raw/master/fonts/fontawesome-webfont.ttf
 mv fontawesome-webfont.ttf ~/.fonts/
 
+# Screenshot
+sudo apt-get install scrot -y
+
 # To change the background
 sudo apt-get install feh -y
 
 # Install zsh
 sudo apt-get install zsh  -y
 chsh -s $(which zsh)
+
+# Numlock
+sudo apt-get install numlockx -y
 
 # Install http://ohmyz.sh/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -33,7 +39,22 @@ sudo apt-get install rofi -y
 mkdir ~/.config/rofi
 curl -o ~/.config/rofi/config https://raw.githubusercontent.com/DaveDavenport/rofi-themes/master/Themes/Arc-Dark.theme
 
-sudo apt-get install xcompmgr -y
+#sudo apt-get install xcompmgr -y
 
+#
+# Powerline fonts
+#
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
 
-
+#
+# Features LiL 
+#
+sudo apt-get install screenfetch -y
+sudo apt-get install tty-clock -y
